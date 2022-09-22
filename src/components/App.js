399e26967afd4828fcaac764936b6cc1bb0549d3
Maureenkamau03 <mkaymaureen@gmail.com>
2022-09-22 16:12:15 +0300

    assigment completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import About from "./About";
+import NavBar from "./NavBar";
+import Home from "./Home";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Home />
+			<About />
+		</div>
+	);
 }
 
 export default App;
